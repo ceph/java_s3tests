0d1ed23aa4109cef4e13ef40ebdcf8427c773aa0
Casey Bodley <cbodley@redhat.com>
2026-03-27 15:14:45 -0400

    bootstrap: try newer gradle version
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -68,7 +68,7 @@ echo " Gradle will be installed under $GRADLEPATH"
 
 # The version is hardcoded on purpose in order to match the 
 # one used for testing in the Ceph test framework Teuthology
-version=6.0.1
+version=9.4.1
 
 if [ ! -d /opt/gradle ]; then
     sudo mkdir ${GRADLEPATH}
