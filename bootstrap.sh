e3d1ea7e75e407347020a24855262556fc0de2c0
J. Eric Ivancich <ivancich@redhat.com>
2026-03-31 17:50:33 -0400

    bootstrap: install Java 8 from adoptium-temurin under rocky linux
    Updates the bootstrap.sh script so it can install a version of Java 8.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    (cherry picked from commit 837ac8fa25ff379902955f2217a6dde288d53755)

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,6 +36,21 @@ if [ -f /etc/debian_version ]; then
         echo "$0: missing required DEB packages. Installing via sudo." 1>&2
         sudo apt-get -y install $missing
     fi
+elif [ -f /etc/rocky-release ]; then
+    # need to check for /etc/rocky-release before /etc/redhat-release
+    # since both present
+    for package in wget unzip epel-release; do
+        if [ "$(rpm -qa $package 2>/dev/null)" == "" ]; then
+            missing="${missing:+$missing }$package"
+        fi
+    done
+    if [ -n "$missing" ]; then
+        echo "$0: missing required RPM packages. Installing via sudo." 1>&2
+        sudo dnf -y install $missing
+    fi
+    sudo dnf -y install adoptium-temurin-java-repository
+    sudo dnf config-manager --set-enabled adoptium-temurin-java-repository
+    sudo dnf -y install temurin-8-jdk
 elif [ -f /etc/fedora-release ]; then
     for package in java-1.8.0-openjdk wget unzip; do
         if [ "$(rpm -qa $package 2>/dev/null)" == "" ]; then
